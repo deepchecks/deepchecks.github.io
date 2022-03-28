cd7c141692444062949a6d67368718a631584bcb
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-03-28 13:15:37 +0000

    Pushing the docs to 0.3/ for branch: 0.3.x, commit 95066b9417c8346fec8236e3d0d5ced426f3b89b

--- 0.3/_static/documentation_options.js
+++ 0.3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'bf69610',
+    VERSION: '95066b9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
