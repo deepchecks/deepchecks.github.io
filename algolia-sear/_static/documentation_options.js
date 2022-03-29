eb5de7acae86f19b8cf2ca803a577c6a7a96538a
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-03-29 07:54:19 +0000

    Pushing the docs to algolia-sear/ for branch: algolia-search, commit e1ce5aeb4803b5b8c427cd521fb9182b4af4584f

--- algolia-sear/_static/documentation_options.js
+++ algolia-sear/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '07ce157',
+    VERSION: 'e1ce5ae',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
