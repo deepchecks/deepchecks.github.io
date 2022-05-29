1bee10498fdb076dc2b6a4f41aaa1421b8d76cbd
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-05-29 14:11:30 +0000

    Pushing the docs to dev/ for branch: main, commit 40df64d489ef1cb8e6b484efc8a8bc2595665c5c

--- dev/_downloads/56f4b0edd5484651af2648f597eeb9c7/plot_train_test_label_drift.py
+++ dev/_downloads/56f4b0edd5484651af2648f597eeb9c7/plot_train_test_label_drift.py
@@ -2,32 +2,6 @@
 """
 Train Test Label Drift
 **********************
-
-This notebooks provides an overview for using and understanding label drift check.
-
-**Structure:**
-
-* `What Is Label Drift? <#what-is-label-drift>`__
-* `Run Check on a Classification Label <#run-check-on-a-classification-label>`__
-* `Run Check on a Regression Label <#run-check-on-a-regression-label>`__
-* `Add a Condition <#run-check>`__
-
-What Is Label Drift?
-========================
-Drift is simply a change in the distribution of data over time, and it is
-also one of the top reasons why machine learning model's performance degrades
-over time.
-
-Label drift is when drift occurs in the label itself.
-
-For more information on drift, please visit our :doc:`drift guide </user-guide/general/drift_guide>`.
-
-How Deepchecks Detects Label Drift
-------------------------------------
-
-This check detects label drift by using :ref:`univariate measures <drift_detection_by_univariate_measure>`
-on the label column.
-
 """
 
 #%%
@@ -41,17 +15,14 @@ from deepchecks.tabular import Dataset
 from deepchecks.tabular.checks import TrainTestLabelDrift
 
 #%%
-# Run Check on a Classification Label
+# Generate data - Classification label
 # ====================================
-
-# Generate data:
-# --------------
 
 np.random.seed(42)
 
 train_data = np.concatenate([np.random.randn(1000,2), np.random.choice(a=[1,0], p=[0.5, 0.5], size=(1000, 1))], axis=1)
 #Create test_data with drift in label:
-test_data = np.concatenate([np.random.randn(1000,2), np.random.choice(a=[1,0], p=[0.35, 0.65], size=(1000, 1))], axis=1)
+test_data = np.concatenate([np.random.randn(1000,2), np.random.choice(a=[1,0], p=[0.35, 0.65], size=(1000, 1))], axis=1) 
 
 df_train = pd.DataFrame(train_data, columns=['col1', 'col2', 'target'])
 df_test = pd.DataFrame(test_data, columns=['col1', 'col2', 'target'])
@@ -65,18 +36,15 @@ df_train.head()
 
 #%%
 # Run Check
-# ===============================
+# =========
 
 check = TrainTestLabelDrift()
 result = check.run(train_dataset=train_dataset, test_dataset=test_dataset)
 result
 
 #%%
-# Run Check on a Regression Label
+# Generate data - Regression label
 # ================================
-
-# Generate data:
-# --------------
 
 train_data = np.concatenate([np.random.randn(1000,2), np.random.randn(1000, 1)], axis=1)
 test_data = np.concatenate([np.random.randn(1000,2), np.random.randn(1000, 1)], axis=1)
@@ -91,15 +59,14 @@ test_dataset = Dataset(df_test, label='target')
 
 #%%
 # Run check
-# ---------
+# =========
 
 check = TrainTestLabelDrift()
 result = check.run(train_dataset=train_dataset, test_dataset=test_dataset)
 result
 
 #%%
-# Add a Condition
-# ===============
+# Add condition
 
 check_cond = TrainTestLabelDrift().add_condition_drift_score_not_greater_than()
 check_cond.run(train_dataset=train_dataset, test_dataset=test_dataset)
