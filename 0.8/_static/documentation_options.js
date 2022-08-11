2e5581d9e1e1eff113e576bcda85e07b3bbf44b1
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-08-11 09:10:02 +0000

    Pushing the docs to 0.8/ for branch: 0.8.x, commit 860754f8b1c71d9f2810906bd82e01d8828a7109

--- 0.8/_static/documentation_options.js
+++ 0.8/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c7b351d',
+    VERSION: '860754f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
