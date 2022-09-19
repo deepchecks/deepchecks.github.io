ba381447a211fef1a7642d8148a9277dc17f438e
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-09-19 12:40:22 +0000

    Pushing the docs to 0.9/ for branch: 0.9.x, commit b2efa6d50db26f288a355118623ea0a11d2c290f

---
+++ 0.9/_downloads/a7f947cf485233fec9fc9bb256ea1be8/plot_model_info.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+.. _plot_tabular_model_info:
+
+Model Info
+**********
+"""
+
+#%%
+
+from sklearn.ensemble import AdaBoostClassifier
+
+from deepchecks.tabular.checks import ModelInfo
+
+#%%
+clf = AdaBoostClassifier(learning_rate=1.2)
+ModelInfo().run(clf)
