900d722f5fcfcb1119b0992755ccffe4aa467278
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-10-16 14:27:45 +0000

    Pushing the docs to dev/ for branch: main, commit 0ac815b026fe940edc142f394501758f8159dcbd

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4a55424',
+    VERSION: '0ac815b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
