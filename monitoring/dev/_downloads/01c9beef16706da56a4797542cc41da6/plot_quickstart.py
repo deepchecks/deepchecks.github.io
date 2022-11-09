1e6d394c01ddcf451ead20fccfb05c73b6610ec8
deepchecks-ci <itay+deepchecks-ci@deepchecks.com>
2022-11-09 14:22:47 +0000

    Pushing the docs to monitoring/dev/ for branch: main, commit e996bfceaf2f82c64b568388faf6626cd4a783ae

--- monitoring/dev/_downloads/01c9beef16706da56a4797542cc41da6/plot_quickstart.py
+++ monitoring/dev/_downloads/01c9beef16706da56a4797542cc41da6/plot_quickstart.py
@@ -82,12 +82,14 @@ read_schema(schema_file_path)
 # through the `reference_proba` argument, enabling computation of probability
 # based metrics such as AUC, log_loss, brier scorer and more.
 
-import os
+import os, random, string
 os.environ['DEEPCHECKS_API_HOST'] = 'https://app.deepchecks.com'
 # note to put the API token in your environment variables. Or alternatively (less recommended):
 # os.environ['DEEPCHECKS_API_TOKEN'] = 'uncomment-this-line-and-insert-your-api-token-here'
 dc_client = DeepchecksClient(host=os.getenv('DEEPCHECKS_API_HOST'), token=os.getenv('DEEPCHECKS_API_TOKEN'))
-model_version = dc_client.create_tabular_model_version(model_name='Airbnb', version_name='ver_1',
+model_name = 'Airbnb' + ''.join(random.choices(string.ascii_letters, k=8))
+
+model_version = dc_client.create_tabular_model_version(model_name=model_name, version_name='ver_1',
                                                        schema_file=schema_file_path,
                                                        feature_importance=feature_importance.to_dict(),
                                                        reference_dataset=ref_dataset,
@@ -144,4 +146,4 @@ model_version.update_batch(samples_to_update=prod_data.index, labels=prod_data[l
 # If we wish to remove the model do free up space for new models we can do it in the following way:
 
 # CAUTION: This will delete the model, all model versions, and all associated datasets.
-dc_client.delete_model('Airbnb')
+dc_client.delete_model(model_name)
